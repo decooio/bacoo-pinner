54f40613b16403fd5cbe1b8a7978c087f9de6398
zibo <ziboilihua@gmail.com>
2022-09-02 10:28:29 +0800

    Fix cors err
    Signed-off-by: zibo <ziboilihua@gmail.com>

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,7 +10,10 @@ import {auth} from "../middlewares/auth";
 
 export const serverStart = async (port: string | number) => {
     const app = express();
-    app.use(cors());
+    app.use(cors({
+        origin:true,
+        credentials: true
+    }));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     app.use('/ping', (req, res) => {
